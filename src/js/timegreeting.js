653d286387db676e13477830d4a62c174f94c9e1
ijmdev <dekkerijm@gmail.com>
2021-05-21 23:08:41 +0200

    fixed timegreeting logic

--- src/js/timegreeting.js
+++ src/js/timegreeting.js
@@ -1,18 +1,24 @@
 const element = document.getElementById("greeting");
 const timeGreeting = () => {
     const today = new Date();
-    const hours =  today.getHours();
-    if (hours >= 17) {
-        element.innerHTML = "Good Evening";
-    } 
-    if (hours >= 12) {
-        element.innerHTML = "Good Afternoon";
-    }
-    if (hours >= 5) {
-        element.innerHTML = "Good Morning";
+    const hours = today.getHours();
+    
+    if (hours <= 24) {
+        element.innerHTML = "Goedenavond";
     }
 
-    element.innerHTML = "Good Evening";
+    if (hours <= 17) {
+        element.innerHTML = "Goedemiddag";
+    }
+
+    if (hours <= 6) {
+        element.innerHTML = "Goedemorgen";
+    }
+
+    if (hours <= 5) {
+        element.innerHTML = "Goedenavond";
+    }
+
 };
 
 export default timeGreeting
